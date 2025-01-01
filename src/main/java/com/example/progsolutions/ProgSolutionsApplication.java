c8e500946006e87f2207f056891f48ebb1e09749
yeosang0508 <yeosang0508@gmail.com>
2025-01-01 23:57:08 +0900

    환경세팅

---
+++ src/main/java/com/example/progsolutions/ProgSolutionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.progsolutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgSolutionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProgSolutionsApplication.class, args);
+    }
+
+}
