433f7c659f0b99baaf68fb60cdf5f8589e6efee6
yeosang0508 <yeosang0508@gmail.com>
2025-02-10 17:54:55 +0900

    programmers_hash_전화번호 목록

---
+++ src/main/java/com/example/Hash/p42577/main.java
@@ -0,0 +1,29 @@
+package com.example.Hash.p42577;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class main {
+}
+class Solution {
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+        Map<String, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < phone_book.length; i++){
+            map.put(phone_book[i], i);
+        }
+
+        for(int i = 0; i < map.size(); i++){
+            for(int j = 0; j < phone_book[i].length(); j++){
+                if(map.containsKey(phone_book[i].substring(0, j))){
+                    return false;
+                }
+            }
+        }
+
+
+        return answer;
+    }
+}
---
+++ src/test/java/com/example/Hash/p42577/Tests.java
@@ -0,0 +1,25 @@
+package com.example.Hash.p42577;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class Tests {
+
+
+    @Test
+    @DisplayName("[\"119\", \"97674223\", \"1195524421\"] ->  false")
+    void t1(){assertThat(new Solution().solution(new String[]{"119", "97674223", "1195524421"})).isEqualTo(false);}
+
+    @Test
+    @DisplayName("[\"123\",\"456\",\"789\"] ->  true")
+    void t2(){assertThat(new Solution().solution(new String[]{"123","456","789"})).isEqualTo(true);}
+
+    @Test
+    @DisplayName("[\"12\",\"123\",\"1235\",\"567\",\"88\"] ->  false")
+    void t3(){assertThat(new Solution().solution(new String[]{"12","123","1235","567","88"})).isEqualTo(false);}
+
+
+
+}
